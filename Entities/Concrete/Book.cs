428f9e788698365f01e6bc7f36b2b33c43b6eda6
Yavuz1997 <yavuzgogus@gmail.com>
2022-11-13 00:22:28 +0300

    Added key annotations

--- Entities/Concrete/Book.cs
+++ Entities/Concrete/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,11 @@ namespace Entities.Concrete
 {
     public class Book
     {
-        public int ISBN { get; set; }
+        [Key]
+        public string ISBN { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
-        public string LanguageID { get; set; }
+        public int LanguageID { get; set; }
         
     }
 }
--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Entities.Concrete
 {
     public class User
     {
+        [Key]
         public int UserID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
