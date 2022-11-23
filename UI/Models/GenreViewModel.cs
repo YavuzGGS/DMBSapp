555acd6ca4b779f7dffdeffce314c58d9fffccb6
Yavuz1997 <yavuzgogus@gmail.com>
2022-11-23 17:12:50 +0300

    Added genre layout for books index

---
+++ UI/Models/GenreViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Entities.Concrete;
+
+namespace UI.Models
+{
+    public class GenreViewModel
+    {
+        public List<Genre> Genres { get; set; }
+    }
+}
---
+++ UI/ViewComponents/GenreListViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using UI.Models;
+
+namespace UI.ViewComponents
+{
+    public class GenreListViewComponent : ViewComponent
+    {
+        private GenreManager _genreManager;
+        public GenreListViewComponent()
+        {
+            _genreManager = new GenreManager();
+        }
+        public ViewViewComponentResult Invoke()
+        {
+            var model = new GenreViewModel
+            {
+                Genres = _genreManager.GetAll()
+            };
+            return View(model);
+
+        }
+    }
+}
