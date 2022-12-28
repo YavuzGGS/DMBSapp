6574596a6605bb991e56ee3c7d9c6db73b42f11a
Yavuz1997 <yavuzgogus@gmail.com>
2022-12-28 18:32:09 +0300

    added genres for the view model

--- UI/Models/BookViewModel.cs
+++ UI/Models/BookViewModel.cs
@@ -6,6 +6,6 @@ namespace UI.Models
     {
         public List<Book> Books { get; set; }
         public Book Book { get; set; }
-        public List<String> Genres { get; set; }
+        public List<string> Genres { get; set; }
     }
 }
