6dd99eed0edf0dd29ed9568cea42a414a968f616
Yavuz1997 <yavuzgogus@gmail.com>
2022-12-29 17:31:05 +0300

    Added key for book_genre

--- Entities/Concrete/Book_Genre.cs
+++ Entities/Concrete/Book_Genre.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    [Keyless]
     public class Book_Genre
     {
+        [Key]
+        public int BookGenreID { get; set; }
         public string BookISBN { get; set; }
         public int GenreID { get; set; }
     }
