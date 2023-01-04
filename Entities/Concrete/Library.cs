9d813473aa84dd517ffc80c3fbf22ce7937ddc5a
Yavuz1997 <yavuzgogus@gmail.com>
2023-01-04 16:07:06 +0300

    Changed entity properties

--- Entities/Concrete/Library.cs
+++ Entities/Concrete/Library.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    [Keyless]
+    
     public class Library
     {
+        [Key]
+        public int EntryID { get; set; }
         public int UserID { get; set; }
-        public int BookID { get; set; }
+        public string BookID { get; set; }
     }
 }
